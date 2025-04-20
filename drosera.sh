07546186953e7f069d0d4fa22d9dda066a3b81f6
Moei <90371338+0xmoei@users.noreply.github.com>
2025-04-20 21:53:52 +0000

    Update drosera.sh

--- drosera.sh
+++ drosera.sh
@@ -64,6 +64,7 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
+sudo rm -f /etc/apt/keyrings/docker.gpg  # Remove existing key file to prevent overwrite prompt
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
